0c0996b55617f7712f7bd869deb28d4bddbf3d01
Sonsoles Alonso <39694036+sonsolesalonsomartinez@users.noreply.github.com>
2023-03-22 11:25:13 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',    
+    'sphinx.ext.napoleon', 
+    'glhmm.graphics',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
