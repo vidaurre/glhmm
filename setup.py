1cc115863ea77ce92489dfd8924bae074dc34094
Nick Yin Larsen <70948370+Nick7900@users.noreply.github.com>
2024-02-28 18:06:47 +0100

    Update setup.py
    Included  'tqdm', 'scikit-image' as dependences

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     author='Diego Vidaurre',
     author_email = "dvidaurre@cfin.au.dk",
     readme = "README.md",
-    install_requires=['scipy','numpy','scikit-learn','matplotlib','numba','seaborn', 'pandas','igraph'],
+    install_requires=['scipy','numpy','scikit-learn','matplotlib','numba','seaborn', 'pandas','igraph', 'tqdm', 'scikit-image'],
     packages=["glhmm"],
     zip_safe=False,
     classifiers=[
