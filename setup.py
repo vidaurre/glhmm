216eb948deca222922a210138cd3f29468f7f6b0
Nick Yin Larsen <70948370+Nick7900@users.noreply.github.com>
2024-03-01 14:46:45 +0100

    Update setup.py
     "statsmodels" will now be automatically installed as dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='glhmm',
-    version='0.2.1',
+    version='0.2.2',
     description='Gaussian Linear Hidden Markov Model',
     url='https://github.com/vidaurre/glhmm',
     author='Diego Vidaurre',
     author_email = "dvidaurre@cfin.au.dk",
     readme = "README.md",
-    install_requires=['scipy','numpy','scikit-learn','matplotlib','numba','seaborn', 'pandas','igraph', 'tqdm', 'scikit-image'],
+    install_requires=['scipy','numpy','scikit-learn','matplotlib','numba','seaborn', 'pandas','igraph', 'tqdm', 'scikit-image','statsmodels'],
     packages=["glhmm"],
     zip_safe=False,
     classifiers=[
