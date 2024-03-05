edab5d00d88ba10c6726975cec87a732ed99d313
sonsolesalonsomartinez <alonsomartinez.sonsoles@gmail.com>
2024-03-05 11:23:31 +0100

    Modify Sphinx version required

--- docs/conf.py
+++ docs/conf.py
@@ -57,3 +57,7 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# Set the required version of Sphinx
+needs_sphinx = '5.0'
