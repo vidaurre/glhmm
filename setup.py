6ee485c36979763f2201a90151d527a4a79c71d4
Nick Yin Larsen <70948370+Nick7900@users.noreply.github.com>
2025-04-07 11:49:40 +0200

    Update setup.py
    Forgot to update the version when I added 'nibabel'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='glhmm',
-    version='0.2.10',
+    version='0.2.11',
     description='Gaussian Linear Hidden Markov Model',
     url='https://github.com/vidaurre/glhmm',
     author='Diego Vidaurre',
